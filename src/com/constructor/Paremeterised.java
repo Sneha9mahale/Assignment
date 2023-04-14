85a6e37706bf564de627f477582106904ba299a3
Rahul <Rahul@DESKTOP-D6BKAO3>
2023-04-14 10:56:59 +0530

    PolyAssignment

--- src/com/constructor/Paremeterised.java
+++ src/com/constructor/Paremeterised.java
@@ -1,12 +1,20 @@
 package com.constructor;
 
 public class Paremeterised {
-	long accountNo;
+	int accountNo;
 	String customerName;
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	Paremeterised(int accountNo,String customerName)
+	{
+		this.accountNo=accountNo;
+		this.customerName=customerName;
+		System.out.println(accountNo);
+		System.out.println(customerName);
 	}
 
+
+	public static void main(String[] args) {
+	Paremeterised p=new Paremeterised(12345,"sneha mahale");
+	
+	}
 }
+
